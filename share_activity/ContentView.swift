004179776606a679bb72dbbfa0af7905dbbac24b
Masashi Aso <maashi.apple.japan@icloud.com>
2019-12-26 16:25:46 +0900

    Initial Commit

---
+++ share_activity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  share_activity
+//
+//  Created by Masashi Aso on 2019/12/26.
+//  Copyright © 2019 Masashi Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
